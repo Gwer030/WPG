8478d2dae7ae61b88954dc38ea6bbb624e4b48b8
Gwer030 <55752245+Gwer030@users.noreply.github.com>
2019-10-16 01:18:14 +0600

    Update app-routing.module.ts

--- Seth/src/app/app-routing.module.ts
+++ Seth/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+import { RulesComponent } from './rules/rules.component';
+import { MapComponent } from './map/map.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'events', component:EventsComponent },
+  {path: 'rules', component:RulesComponent},
+  {path: 'map',component:MapComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
