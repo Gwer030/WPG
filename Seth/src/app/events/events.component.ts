fb7aa52e6418a5fe9e79d0da9c9561a9b86620c7
Gwer030 <55752245+Gwer030@users.noreply.github.com>
2019-10-16 01:18:28 +0600

    Create events.component.ts

---
+++ Seth/src/app/events/events.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  
+  selector: 'app-events',
+  template: `
+  <div style="text-align: center">
+    <h1>
+    В этой секции будут описаны события происходящие в ВПИ
+    <h1>
+
+  `,
+  styles: []
+})
+export class EventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
